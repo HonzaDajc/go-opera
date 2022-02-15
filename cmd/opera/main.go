fd93cdb3920100dd8cd2ab8c065adbc59869c016
HonzaDajc <jan.dajc@gmail.com>
2022-02-15 20:56:18 +0100

    Force-load the tracer engines to trigger registration

--- cmd/opera/main.go
+++ cmd/opera/main.go
@@ -5,6 +5,10 @@ import (
 	"os"
 
 	"github.com/Fantom-foundation/go-opera/cmd/opera/launcher"
+
+	// Force-load the tracer engines to trigger registration
+	_ "github.com/Fantom-foundation/go-opera/tracers/js"
+	_ "github.com/Fantom-foundation/go-opera/tracers/native"
 )
 
 func main() {
