5fe92ff3060e0a906dde4183eceb0f39a27586b2
alex <rusdev.alex@gmail.com>
2023-01-10 17:25:19 +1000

    topicsdb.Index interface

--- gossip/evmstore/store.go
+++ gossip/evmstore/store.go
@@ -43,7 +43,7 @@ type Store struct {
 
 	EvmDb    ethdb.Database
 	EvmState state.Database
-	EvmLogs  *topicsdb.Index
+	EvmLogs  topicsdb.Index
 	Snaps    *snapshot.Tree
 
 	cache struct {
@@ -78,7 +78,7 @@ func NewStore(dbs kvdb.DBProducer, cfg StoreConfig) *Store {
 	}
 
 	s.initEVMDB()
-	s.EvmLogs = topicsdb.New(dbs)
+	s.EvmLogs = topicsdb.NewWithThreadPool(dbs)
 	s.initCache()
 
 	return s
